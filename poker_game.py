f92948fe60639b8823957e66441b17698cc7e701
karmansandhanwalia-art <karmansandhanwalia@gmail.com>
2026-06-18 17:15:03 +0000

    Add working poker game

--- poker_game.py
+++ poker_game.py
@@ -1,1 +1,44 @@
+import random
 
+cards = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+def deal_card():
+    return random.choice(cards)
+
+def play_round():
+    player_card = deal_card()
+    computer_card = deal_card()
+
+    print("\nYour card:", player_card)
+    print("Computer card:", computer_card)
+
+    values = {
+        "A": 14,
+        "K": 13,
+        "Q": 12,
+        "J": 11,
+        "10": 10,
+        "9": 9,
+        "8": 8,
+        "7": 7,
+        "6": 6,
+        "5": 5,
+        "4": 4,
+        "3": 3,
+        "2": 2
+    }
+
+    if values[player_card] > values[computer_card]:
+        print("You win!")
+    elif values[player_card] < values[computer_card]:
+        print("Computer wins!")
+    else:
+        print("Tie!")
+
+while True:
+    play_round()
+    again = input("Play again? yes/no: ").lower()
+
+    if again != "yes":
+        print("Thanks for playing!")
+        break
